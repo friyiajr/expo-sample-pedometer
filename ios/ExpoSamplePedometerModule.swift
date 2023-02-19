0dd0f95ce15722e815c81f93175a4123371675d9
Daniel Friyia <dfriyia@gmail.com>
2023-02-18 21:29:56 -0500

    iOS Part Complete

--- ios/ExpoSamplePedometerModule.swift
+++ ios/ExpoSamplePedometerModule.swift
@@ -1,13 +1,28 @@
 import ExpoModulesCore
+import CoreMotion
 
 public class ExpoSamplePedometerModule: Module {
+    
+    let kOnStepCounted = "onStepCounted"
 
-  public func definition() -> ModuleDefinition {
+    public func definition() -> ModuleDefinition {
+        let pedometer = CMPedometer()
 
-    Name("ExpoSamplePedometer")
+        Name("ExpoSamplePedometer")
+        
+        Events(kOnStepCounted)
 
-    Function("hello") {
-      return "Hello world! 👋"
+        Function("requestPermissions") {
+            print("Only needed in Android")
+        }
+        
+        Function("startSendingData") {
+            pedometer.startUpdates(from: Date()) { pedometerData, error in
+            guard let pedometerData = pedometerData, error == nil else { return }
+                self.sendEvent(self.kOnStepCounted, [
+                   "step": pedometerData.numberOfSteps.intValue
+                ])
+            }
+        }
     }
-  }
 }
